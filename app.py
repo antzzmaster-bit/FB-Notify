e91a350fca3c48b4160247a951dab8768c1e6c56
antzzmaster-bit <antzzmaster@gmail.com>
2026-07-02 06:28:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "FB Notify is running!"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    print(request.json)
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
